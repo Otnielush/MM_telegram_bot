d5127f16ee239e2f8e634dba1ce265baddc29b29
Ivan Sharonov <48245524+isharonov@users.noreply.github.com>
2023-09-03 00:33:23 +0300

    MMTB-9 Add Webhook Secret Token generator

--- youtuber/utils.py
+++ youtuber/utils.py
@@ -1,6 +1,9 @@
 import requests
 from mmtelegrambot.settings import TELEGRAM_API_URL
 
+import string
+import random
+
 
 def escape_str(text):
     translate_table = str.maketrans({
@@ -31,3 +34,11 @@ def escape_str(text):
 
 def send_api_request(method, data):
     return requests.post(TELEGRAM_API_URL + method, data)
+
+
+def get_webhook_secret_token(length=64):
+    res = ''.join(random.choices(string.ascii_uppercase +
+                                 string.ascii_lowercase +
+                                 string.digits, k=length))
+
+    return res
