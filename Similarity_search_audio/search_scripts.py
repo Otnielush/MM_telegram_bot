1e1c647b9793cbd5a52cf7aefc02a1fe4ac7c137
Otnielush <Otniel.Elkayam@gmail.com>
2025-06-18 23:27:20 +0300

    h search

--- Similarity_search_audio/search_scripts.py
+++ Similarity_search_audio/search_scripts.py
@@ -3,12 +3,6 @@ import openai
 from mmtelegrambot.settings import OPENAI_KEY, NEO4J_URI, NEO4J_DB, NEO4J_AUTH, NEO4J_embd_index
 
 
-# from dotenv import load_dotenv, find_dotenv
-# load_dotenv()
-# # prod env if DEBUG
-# if os.getenv("DEBUG", "True").lower() == 'false':
-#     load_dotenv(find_dotenv(".env.prod"), override=True)
-
 client = openai.OpenAI(api_key=OPENAI_KEY)
 
 
@@ -23,6 +17,26 @@ query_sim = """CALL db.index.vector.queryNodes($vector_index_name, 30, $query_ve
 RETURN node.lesson_name AS lesson_name, node.time_start AS start, node.time_end AS end, node.upload_date AS upload_date, 
 node.youtube_id AS youtube_id, round(score * 1000) / 1000 AS search_score LIMIT $top_k
 """
+
+hybrid_search_example = (
+        "CALL { "
+        "CALL db.index.vector.queryNodes($index, $k, $embedding) "
+        "YIELD node, score "
+        "WITH collect({node:node, score:score}) AS nodes, max(score) AS max "
+        "UNWIND nodes AS n "
+        # We use 0 as min
+        "RETURN n.node AS node, (n.score / max) AS score UNION "
+        "CALL db.index.fulltext.queryNodes($keyword_index, $query, {limit: $k}) "
+        "YIELD node, score "
+        "WITH collect({node:node, score:score}) AS nodes, max(score) AS max "
+        "UNWIND nodes AS n "
+        # We use 0 as min
+        "RETURN n.node AS node, (n.score / max) AS score "
+        "} "
+        # dedup
+        "WITH node, max(score) AS score ORDER BY score DESC LIMIT $k "
+    )
+
 
 def similarity_search(query, top_k=10):
     embeddings = get_embeddings(query)[0]
