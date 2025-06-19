f2113c38cde61d360ec68b36ad8b23755f339c64
Ivan Sharonov <48245524+isharonov@users.noreply.github.com>
2025-06-19 10:33:19 +0300

    fix: update property access method for result parsing and add full-text index in .env example

--- cleaner/utils.py
+++ cleaner/utils.py
@@ -9,9 +9,9 @@ def save_result(message_id, user_id, chat_id, sent_at, text, result):
         results = []
         for item in result:
             results.append({
-                "name": item["lesson_name"],
-                "yt_id": item["youtube_id"],
-                "score": item["search_score"]
+                "name": item.lesson_name,
+                "yt_id": item.youtube_id,
+                "score": item.search_score
             })
         try:
             Question.objects.create(
@@ -32,11 +32,11 @@ def make_result_message(result):
     if len(result):
         item_strings = []
         for item in result:
-            title = item["lesson_name"]
-            youtube_id = item["youtube_id"]
-            start_time = round(item["start"])
-            end_time = round(item["end"]) if item["end"] > 0 else None
-            upload_date = item["upload_date"]
+            title = item.lesson_name
+            youtube_id = item.youtube_id
+            start_time = round(item.start)
+            end_time = round(item.end) if item.end > 0 else None
+            upload_date = item.upload_date
             msg = make_youtube_link_msg(title, youtube_id, start_time, end_time, upload_date)
             item_strings.append(msg)
         return '\n\n'.join(item_strings)
